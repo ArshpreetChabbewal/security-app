a927d887f11840eae00c18a5aa5d12f078358765
anjalig21 <anjaligta@outlook.com>
2021-09-18 13:30:31 -0400

    set up and configured database

---
+++ server/db.js
@@ -0,0 +1,17 @@
+import { initializeApp } from "firebase/app";
+const dotenv = require('dotenv');
+dotenv.config();
+
+// Firebase configuration
+const firebaseConfig = {
+  apiKey: process.env.apiKey,
+  authDomain: process.env.authDomain,
+  projectId: process.env.projectId,
+  storageBucket: process.env.storageBucket,
+  messagingSenderId: process.env.messagingSenderId,
+  appId: process.env.appId,
+  measurementId: process.env.measurementId
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
