31f5dfab9917b6fbe6bb632b64fef72c05194ffa
Anjali <anjali@wsib-innovation.ca>
2021-09-18 12:36:20 -0400

    basic setup done

---
+++ client/src/App.js
@@ -0,0 +1,9 @@
+import "./App.css";
+import ReactDOM from "react-dom";
+import { Router, Route, Link } from "react-router"; // npm install this
+
+function App() {
+  return <div className="App"></div>;
+}
+
+export default App;
