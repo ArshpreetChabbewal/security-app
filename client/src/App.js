5fbbb882d24a147920c0c9565367a735d15fbcc3
Arshpreet Chabbewal <arsh.c321@gmail.com>
2021-09-18 14:06:53 -0400

    Set up components

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,21 @@
 import "./App.css";
 import ReactDOM from "react-dom";
-import { Router, Route, Link } from "react-router"; // npm install this
+import { Router, Route, Link } from "react-router";
+import Login from './Components/Login/Login';
+import Alert from './Components/Alert/Alert';
+import Home from './Components/Home/Home';
+import Signup from "./Components/Login/Signup";
+import Map from './Components/Map/Map';
+
 
 function App() {
-  return <div className="App"></div>;
+  return <div className="App">
+    <Login/>
+    <Signup/>
+    <Alert/>
+    <Home/>
+    <Map/>
+  </div>;
 }
 
 export default App;
